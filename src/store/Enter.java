2850a257da3ec49605197789703408aeb3418b13
Dima <dimatsaruk@ukr.net>
2015-09-03 22:08:43 +0300

    add 4th case

--- src/store/Enter.java
+++ src/store/Enter.java
@@ -1,29 +1,41 @@
-
 package store;
 
 public class Enter {
+
     int password = 0;
+
     public void enter() {
         System.out.println("Welcome to iStore, please enter you login and password...");
     }
+
     void authorization(int i) {
         //this.password = i;
-        if (i==1) 
+        if (i == 1) {
             System.out.println("Authorization successful, Please select product category.");
-            else System.out.println("Unfortunately you enter the wrong password, Try again .");
+        } else {
+            System.out.println("Unfortunately you enter the wrong password, Try again .");
         }
-    void choseProduct (int i) {
-             if (i==1) System.out.println("You have chosen a product number 1");
-        else if(i==2) System.out.println("You have chosen a product number 2");
-        else if(i==3) System.out.println("You have chosen a product number 3");
+    }
+
+    void choseProduct(int i) {
+        if (i == 1) {
+            System.out.println("You have chosen a product number 1");
+        } else if (i == 2) {
+            System.out.println("You have chosen a product number 2");
+        } else if (i == 3) {
+            System.out.println("You have chosen a product number 3");
+        } else if (i == 4) {
+            System.out.println("You have chosen a product number 4");
+        }
     }
 }
+
 class Test {
+
     public static void main(String[] args) {
-        Enter store = new Enter() ;
+        Enter store = new Enter();
         store.enter();
         store.authorization(1);
         store.choseProduct(2);
     }
 }
-
--- src/store/Store.java
+++ src/store/Store.java
@@ -10,10 +10,7 @@ public class Store {
 
     public static void main(String[] args) {
         
-    
-        Scanner num = new Scanner(System.in);
-       // System.out.println(num);
-        
+    InputStreamReader isr = new InputStreamReader(Sy)
     }
 }
     
