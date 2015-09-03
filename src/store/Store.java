283d12ad19f5f34ab9b9ce7a0d3c8888c86354b2
Dima <dimatsaruk@ukr.net>
2015-09-03 23:05:06 +0300

    Scanner add

--- src/store/Store.java
+++ src/store/Store.java
@@ -1,17 +1,16 @@
-
 package store;
 
-import java.util.Scanner;
-
-
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Store {
 
-    public static void main(String[] args) {
-     // hello Dima
-    InputStreamReader isr = new InputStreamReader(Sy)
+    public static void main(String[] args) throws IOException {
+        // hello Dima
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(isr);
+        String name = reader.readLine();
+
     }
 }
-    
-
