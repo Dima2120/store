47b62d2821d1e0b43a247d7ca6c0bf50c1b2d679
Dima <dimatsaruk@ukr.net>
2015-09-03 22:01:08 +0300

    create project structure

---
+++ src/store/Enter.java
@@ -0,0 +1,29 @@
+
+package store;
+
+public class Enter {
+    int password = 0;
+    public void enter() {
+        System.out.println("Welcome to iStore, please enter you login and password...");
+    }
+    void authorization(int i) {
+        //this.password = i;
+        if (i==1) 
+            System.out.println("Authorization successful, Please select product category.");
+            else System.out.println("Unfortunately you enter the wrong password, Try again .");
+        }
+    void choseProduct (int i) {
+             if (i==1) System.out.println("You have chosen a product number 1");
+        else if(i==2) System.out.println("You have chosen a product number 2");
+        else if(i==3) System.out.println("You have chosen a product number 3");
+    }
+}
+class Test {
+    public static void main(String[] args) {
+        Enter store = new Enter() ;
+        store.enter();
+        store.authorization(1);
+        store.choseProduct(2);
+    }
+}
+
---
+++ src/store/Store.java
@@ -0,0 +1,20 @@
+
+package store;
+
+import java.util.Scanner;
+
+
+
+
+public class Store {
+
+    public static void main(String[] args) {
+        
+    
+        Scanner num = new Scanner(System.in);
+       // System.out.println(num);
+        
+    }
+}
+    
+
