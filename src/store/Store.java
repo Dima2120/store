bfef59f369d8c4270a02052f9708826d52d0fe07
Dima2120 <DimaTsaruk@ukr.net>
2015-09-03 22:53:46 +0300

    added message

--- src/store/Store.java
+++ src/store/Store.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Store {
 
     public static void main(String[] args) {
-        
+     // hello Dima
     InputStreamReader isr = new InputStreamReader(Sy)
     }
 }
